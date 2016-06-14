0daa89302cb9f8349475ea4390d74b284c5f2da4
arunasank <aruna@mapbox.com>
2016-06-13 21:34:46 -0400

    cleanup

--- test/termops.replaceTokenPermutations.test.js
+++ test/termops.replaceTokenPermutations.test.js
@@ -4,12 +4,12 @@ var clone = function(d) { return JSON.parse(JSON.stringify(d)); }
 var token = require('../lib/util/token');
 
 test('termops.replaceTokenPermutations', function(assert) {
-    var replacer = token.createReplacer({'street':'st', 'north':'n'});
-    console.log(termops.replaceTokenPermutations(replacer, 'North Main Street'));
+    var replacer = token.createReplacer({'Street':'St', 'North':'N'});
     assert.deepEqual(termops.replaceTokenPermutations(replacer, 'North Main Street', [
         ['North Main Street'],
         ['N Main Street'],
         ['North Main St'],
         ['N Main St']
     ]));
+    assert.end();
 });
