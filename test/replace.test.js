095d6527d6410181073511f2b813d97bdbc740ff
Aarthy Chandrasekhar <kcaarthy@gmail.com>
2016-06-14 12:20:15 -0400

    refactor

--- test/replace.test.js
+++ test/replace.test.js
@@ -200,7 +200,7 @@ var tokens = token.createReplacer({
     "Walkway": "Wlky",
     "West": "W",
     "San Francisco": "sf"
-});
+}, false);
 
 test('token replacement', function(q) {
     q.deepEqual(token.replaceToken(tokens, 'fargo street northeast, san francisco'),'fargo St NE, sf');
@@ -216,7 +216,7 @@ test('replacer', function(q) {
     var rep = token.createReplacer({
         'Road': 'Rd',
         'Street': 'St'
-    });
+    }, false);
     q.deepEqual(rep.map(function(r) { return r.named; }), [false, false]);
     q.deepEqual(rep.map(function(r) { return r.to; }), ['$1Rd$2', '$1St$2']);
     q.deepEqual(rep.map(function(r) { return r.from.toString(); }), ['/(\\W|^)Road(\\W|$)/gi', '/(\\W|^)Street(\\W|$)/gi']);
@@ -224,7 +224,7 @@ test('replacer', function(q) {
     rep = token.createReplacer({
         'Maréchal': 'Mal',
         'Monsieur': 'M'
-    });
+    }, false);
     q.deepEqual(rep.map(function(r) { return r.named; }), [false, false, false]);
     q.deepEqual(rep.map(function(r) { return r.to; }), ['$1Mal$2', '$1Mal$2', '$1M$2']);
     q.deepEqual(rep.map(function(r) { return r.from.toString(); }), ['/(\\W|^)Maréchal(\\W|$)/gi', '/(\\W|^)Marechal(\\W|$)/gi', '/(\\W|^)Monsieur(\\W|$)/gi']);
@@ -237,7 +237,8 @@ test('named/numbered group replacement', function(q) {
         "abc": "xyz",
         "(1\\d+)": "@@@$1@@@",
         "(?<number>2\\d+)": "###${number}###"
-    });
+    }, false);
+    console.log('forward', tokens);
     q.deepEqual(token.replaceToken(tokens, 'abc 123 def'), 'xyz @@@123@@@ def');
     q.deepEqual(token.replaceToken(tokens, 'abc 234 def'), 'xyz ###234### def');
     q.end();
@@ -245,7 +246,7 @@ test('named/numbered group replacement', function(q) {
 
 test('throw on mixed name/num replacement groups', function(q) {
     q.throws(function() {
-        token.createReplacer({ "(abc)(?<namedgroup>def)": "${namedgroup}$1" });
+        token.createReplacer({ "(abc)(?<namedgroup>def)": "${namedgroup}$1" }, false);
     });
     q.end();
 });
@@ -256,8 +257,22 @@ test('reverse permutation', function(q) {
         'street': 'st',
         'road': 'rd'
     }
-    var result = token.createReverseReplacer(tokens);
+    var result = token.createReplacer(tokens, true);
     q.deepEqual(result.map(function(r) { return r.to; }), ['$1saint$2','$1street$2','$1road$2']);
     q.deepEqual(result.map(function(r) { return r.from.toString(); }), [ '/(\\W|^)st(\\W|$)/gi', '/(\\W|^)st(\\W|$)/gi', '/(\\W|^)rd(\\W|$)/gi' ]);
+    q.end();
+});
+
+test('named/numbered group replacement for reverse permutation', function(q) {
+    var tokens = {
+        'saint': 'st',
+        'street': 'st',
+        'road': 'rd'
+    };
+    var result = token.createReplacer(tokens, true);
+
+    q.deepEqual(token.replaceToken(result, 'fargo rd'),'fargo road');
+    q.deepEqual(token.replaceToken(result, 'fargo st'),'fargo saint');
+    q.deepEqual(token.replaceToken(result, 'stwise'),'stwise');
     q.end();
 });
