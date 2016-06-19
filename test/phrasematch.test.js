9481f7e55e5864c71a024fdadc8f497e49e04006
arunasank <aruna@mapbox.com>
2016-06-18 23:45:54 -0400

    Tests for new deduplicate

--- test/phrasematch.test.js
+++ test/phrasematch.test.js
@@ -3,26 +3,25 @@ var phrasematch = require('../lib/phrasematch');
 var deDuplicateSubqueries = phrasematch.deDuplicateSubqueries;
 
 tape('Dedupe subqueries array', function(assert) {
-var obj = [ [ 'avenue', 'du', '18e', 'régiment' ],
-          [ 'avenue', 'du', '18e'],
-          [ 'du', '18e', 'régiment' ],
-          [ 'avenue', 'du'],
-          ['avenue', 'du', '18e']
-        ];
-        for (var i = 0; i < obj.length; i++) {
-            obj[0].ender = true,
-            obj[0].mask = 15,
-            obj[1].ender = false,
-            obj[1].mask = 14,
-            obj[2].ender = true,
-            obj[2].mask = 3,
-            obj[3].ender = false,
-            obj[3].mask = 3,
-            obj[4].ender = false,
-            obj[4].mask = 14;
-        } 
+var obj = [[[ 'avenue', 'du', '18e', 'régiment' ],
+            [ 'avenue', 'du', '18e'],
+            [ 'du', '18e', 'régiment' ],
+            [ 'avenue', 'du'],
+            ['avenue', 'du', '18e']]];
+var enderArray = [true, false, true, false, false];
+var maskArray = [15,14,3,3,14];
+    for(var j=0; j < obj.length; j++) {
+        var objInner = obj[j];
+        for (var i = 0; i < objInner.length; i++) {
+            objInner.ender = enderArray[j];
+            objInner.mask = maskArray[j];
+        }
+    }
   var result = deDuplicateSubqueries(obj);
-  var expected = [ [ 'avenue', 'du', '18e', 'régiment' ], [ 'avenue', 'du', '18e' ], [ 'du', '18e', 'régiment' ], [ 'avenue', 'du' ] ];
+  var expected = [[[ 'avenue', 'du', '18e', 'régiment' ]],
+                   [[ 'avenue', 'du', '18e' ]],
+                   [[ 'du', '18e', 'régiment' ]],
+                   [[ 'avenue', 'du' ]]];
 
   assert.deepEqual(JSON.stringify(result),JSON.stringify(expected), 'ok de-duped correctly');
   assert.end()
