65624a089eca05d636fa1df5adb76e701354d8d3
arunasank <aruna@mapbox.com>
2016-06-20 12:43:48 +0530

    lintpliss

--- test/deduplicate.subqueries.test.js
+++ test/deduplicate.subqueries.test.js
@@ -3,29 +3,20 @@ var phrasematch = require('../lib/phrasematch');
 var deDuplicateSubqueries = phrasematch.deDuplicateSubqueries;
 
 tape('Dedupe subqueries array', function(assert) {
-    var obj = [
-                [
-                  [ 'avenue', 'du', '18e', 'régiment' ],
+    var obj = [[[ 'avenue', 'du', '18e', 'régiment' ],
                   [ 'avenue', 'du', '18e']
-                ],
-                [
+                ],[
                   [ 'du', '18e', 'régiment' ],
                   [ 'avenue', 'du']
-                ],
-                [
+                ],[
                   ['avenue', 'du', '18e']
-                ]
-              ];
-    var enderArray = [
+                ]];
+    var enderArray = [[true, false],
                         [true, false],
-                        [true, false],
-                        [false]
-                     ];
-    var maskArray = [
-                      [15,14],
+                        [false]];
+    var maskArray = [[15,14],
                       [3,3],
-                      [14]
-                    ];
+                      [14]];
     for (var j=0; j < obj.length; j++) {
         var objInner = obj[j];
         for (var i = 0; i < objInner.length; i++) {
@@ -34,16 +25,12 @@ tape('Dedupe subqueries array', function(assert) {
         }
     }
     var result = deDuplicateSubqueries(obj);
-    var expected = [
-                [
-                  [ 'avenue', 'du', '18e', 'régiment' ],
-                  [ 'avenue', 'du', '18e']
-                ],
-                [
-                  [ 'du', '18e', 'régiment' ],
-                  [ 'avenue', 'du']
-                ]
-                ];
+    var expected = [[[ 'avenue', 'du', '18e', 'régiment' ],
+                     [ 'avenue', 'du', '18e']
+                    ],[
+                      [ 'du', '18e', 'régiment' ],
+                      [ 'avenue', 'du']
+                    ]];
 
     assert.deepEqual(JSON.stringify(result),JSON.stringify(expected), 'ok de-duped correctly');
     assert.end()
