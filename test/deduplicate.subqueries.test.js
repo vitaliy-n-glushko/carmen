2381e62ce8fec1d12e97f71ac49f9c7e88244062
arunasank <aruna@mapbox.com>
2016-06-22 15:41:16 +0530

    Make tests nicer + add text key

--- test/deduplicate.subqueries.test.js
+++ test/deduplicate.subqueries.test.js
@@ -3,34 +3,26 @@ var phrasematch = require('../lib/phrasematch');
 var deDuplicateSubqueries = phrasematch.deDuplicateSubqueries;
 
 tape('Dedupe subqueries array', function(assert) {
-    var obj = [[[ 'avenue', 'du', '18e', 'régiment' ],
-                  [ 'avenue', 'du', '18e']
-                ],[
-                  [ 'du', '18e', 'régiment' ],
-                  [ 'avenue', 'du']
-                ],[
-                  ['avenue', 'du', '18e']
-                ]];
-    var enderArray = [[true, false],
-                        [true, false],
-                        [false]];
-    var maskArray = [[15,14],
-                      [3,3],
-                      [14]];
-    for (var j=0; j < obj.length; j++) {
-        var objInner = obj[j];
+    var subqueriesCollection = [ [['avenue', 'du', '18e', 'régiment'],[ 'avenue', 'du', '18e']],
+                [['du', '18e', 'régiment'],['avenue', 'du']],
+                [['avenue', 'du', '18e']]
+              ];
+    var enderArray = [[true, false],[true, false],[false]];
+    var maskArray = [[15,14],[3,3],[14]];
+    var textArray = [['avenue du 18e régiment', 'avenue du 18e'],['du 18e régiment', 'avenue du'],['avenue du 18e']];
+
+    for (var j=0; j < subqueriesCollection.length; j++) {
+        var objInner = subqueriesCollection[j];
         for (var i = 0; i < objInner.length; i++) {
-            obj[j][i].ender = enderArray[j][i];
-            obj[j][i].mask = maskArray[j][i];
+            subqueriesCollection[j][i].ender = enderArray[j][i];
+            subqueriesCollection[j][i].mask = maskArray[j][i];
+            subqueriesCollection[j][i].text = textArray[j][i];
         }
     }
-    var result = deDuplicateSubqueries(obj);
-    var expected = [[[ 'avenue', 'du', '18e', 'régiment' ],
-                     [ 'avenue', 'du', '18e']
-                    ],[
-                      [ 'du', '18e', 'régiment' ],
-                      [ 'avenue', 'du']
-                    ]];
+    var result = deDuplicateSubqueries(subqueriesCollection);
+    var expected = [ [[ 'avenue', 'du', '18e', 'régiment' ],[ 'avenue', 'du', '18e']],
+                     [[ 'du', '18e', 'régiment' ],[ 'avenue', 'du']]
+                   ];
 
     assert.deepEqual(JSON.stringify(result),JSON.stringify(expected), 'ok de-duped correctly');
     assert.end()
@@ -38,26 +30,26 @@ tape('Dedupe subqueries array', function(assert) {
 
 tape('Dedupe subqueries array variation #2', function(assert) {
     var subqueriesCollection = [];
-
     subqueriesCollection.push([['main','st']]);
-    subqueriesCollection[0][0].ender = false;
-    subqueriesCollection[0][0].mask = 9;
     subqueriesCollection.push([['lake','st'],['wall', 'st'],['main','st']]);
-    subqueriesCollection[1][0].ender = false;
-    subqueriesCollection[1][0].mask = 5;
-    subqueriesCollection[1][1].ender = true;
-    subqueriesCollection[1][1].mask = 3;
-    subqueriesCollection[1][2].ender = false;
-    subqueriesCollection[1][2].mask = 9;
     subqueriesCollection.push([['lake','st'],['wall', 'st'],['main','st']]);
-    subqueriesCollection[2][0].ender = false;
-    subqueriesCollection[2][0].mask = 5;
-    subqueriesCollection[2][1].ender = true;
-    subqueriesCollection[2][1].mask = 3;
-    subqueriesCollection[2][2].ender = false;
-    subqueriesCollection[2][2].mask = 9;
-    var deduped = deDuplicateSubqueries(subqueriesCollection);
-    assert.deepEqual(JSON.stringify(deduped), JSON.stringify([[['main','st']],[['lake','st'],['wall','st']]]), 'ok de-duped correctly');
+
+    var enderArray = [[false],[false,true, false],[false, true, false]];
+    var maskArray = [[9],[5,3,9],[5,3,9]];
+    var textArray = [['main st'],['lake st', 'wall st', 'main st'],['lake st', 'wall st', 'main st']];
+
+    for (var j=0; j < subqueriesCollection.length; j++) {
+        var objInner = subqueriesCollection[j];
+        for (var i = 0; i < objInner.length; i++) {
+            subqueriesCollection[j][i].ender = enderArray[j][i];
+            subqueriesCollection[j][i].mask = maskArray[j][i];
+            subqueriesCollection[j][i].text = textArray[j][i];
+        }
+    }
+
+    var result = deDuplicateSubqueries(subqueriesCollection);
+    var expected = [[['main','st']],[['lake','st'],['wall','st']]];
+    assert.deepEqual(JSON.stringify(result), JSON.stringify(expected), 'ok de-duped correctly');
     assert.end()
 });
 
@@ -67,10 +59,15 @@ tape('Dedupe arrays with tokens - having same mask and ender but different text'
 
     var enderArray = [true, true, false, true, false, false, false, true, false];
     var maskArray = [7,7,3,6,3,1,2,4,1];
+    var textArray = [['23-414 beach street','23### beach street', '23-414 beach', 'beach street', '23### beach', '23-414', 'beach', 'street', '23###']];
 
-    for (var j=0; j < subqueriesCollection[0][0].length; j++) {
-        subqueriesCollection[0][0][j].ender = enderArray[j];
-        subqueriesCollection[0][0][j].mask = maskArray[j];
+    for (var j=0; j < subqueriesCollection.length; j++) {
+        var objInner = subqueriesCollection[j];
+        for (var i = 0; i < objInner.length; i++) {
+            subqueriesCollection[j][i].ender = enderArray[j][i];
+            subqueriesCollection[j][i].mask = maskArray[j][i];
+            subqueriesCollection[j][i].text = textArray[j][i];
+        }
     }
 
     var result = deDuplicateSubqueries(subqueriesCollection);
